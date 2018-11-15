9e306000ff2818188fc82afe1587ed88be46d031
brent.metcalf <brent.metcalf@healthcatalyst.com>
2018-11-15 02:01:02 -0700

    Adding base files to project

---
+++ GeneBankCreateBTree.java
@@ -0,0 +1,5 @@
+public class GeneBankCreateBTree {
+	public static void main(String args[]) {
+		System.out.print("Input program started.");
+	}
+}
---
+++ GeneBankSearch.java
@@ -0,0 +1,5 @@
+public class GeneBankSearch {
+	public static void main(String args[]) {
+		System.out.print("Search program started.");
+	}
+}
