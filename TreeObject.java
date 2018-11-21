bf036776b66c042caab9a0dfc34aea35671a49f1
tylermathern <math3rn@gmail.com>
2018-11-21 12:25:57 -0700

    worked out the constructor

--- TreeObject.java
+++ TreeObject.java
@@ -1,3 +1,25 @@
 public class TreeObject {
-	long sequence;
+	Long key;
+
+	public TreeObject(String sequence){
+		String binaryVal = "";
+		for(int i = 0; i < sequence.length(); i++){
+			char charVal = sequence.charAt(i);
+			
+			if(charVal == 'A'){
+				binaryVal += "00";
+			}else if(charVal == 'C'){
+				binaryVal += "01";
+			}else if(charVal == 'G'){
+				binaryVal += "10";
+			}else if(charVal == 'T'){
+				binaryVal += "11";
+			}else{
+				break;
+			}
+		}
+
+		key = Long.parseLong(binaryVal);
+
+	}
 }
