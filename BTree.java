f72d493374994041b2dcb0c3fdc5bdaae22450af
mustelidae1 <othm@live.com>
2018-11-26 23:34:24 -0700

    Small fixes

--- BTree.java
+++ BTree.java
@@ -8,51 +8,9 @@ public class BTree {
 
 	public BTree(int degree){
 		t = degree;
-		nodes = new ArrayList<BTreeNode>();
+		//nodes = new ArrayList<BTreeNode>();
 		root = new BTreeNode(true, t);
 	}
-
-    // NOTE: I'm assuming that values (from BTreeNode) is an arrayList
-    // Also I'm interacting directly with the values arrayLists, which eventually we may want to 
-    // want to wrap in methods within the BTreeNode object instead 
-    /*private void split(BTreeNode node) {
-
-        // Take all values to the right of the middle and add them to a new node 
-        BTreeNode newRightNode = new BTreeNode(); // TODO: add necessary constructor parameters
-        for (int i = t+1; i < (2 * t); i++) { 
-            newRightNode.values.add(node.values.remove[i]); 
-        }
-
-        // Remove the key that will be moved up 
-        TreeObject middleObject = Object.values.remove(t); 
-        
-        // Check if parent exits 
-        if (node.parent != null) {
-            int index = 0; 
-            // Find correct spot for the object in the parent node and add it there 
-            while (index < node.parent.values.size()) {
-                if (node.parent.values[index].compareTo(middleObject) == 1 || index == node.parent.values.size()) {  
-                    node.parent.values.add(index, middleObject); 
-                    node.parent.children.add(index+1, newRightNode); 
-                    newRightNode.parent = node.parent; 
-                    break; 
-                } 
-                index++; 
-            }
-        } else {
-            // Create a new parent node and add the object 
-            BTreeNode newParent = new BTreeNode(); // TODO: add necessary constructor parameters 
-            newParent.values.add(middleObject); 
-
-            // Add the split children to the new parent node 
-            newParent.children.add(node); 
-            newParent.children.add(newRightNode);
-            
-            // Set parent pointers
-            node.parent = newParent; 
-            newRightNode.parent = newParent; 
-        }
-	}*/
 	
     private void split(BTreeNode parent, int i) {
 		// create new right node and get reference to the node to be split 
@@ -67,28 +25,20 @@ public class BTree {
 		// Move children from the left node to the right node if necessary 
 		if (!leftNode.leaf) {
 			for (int j = 0; j < t; j++) {
-				rightNode.children.add(j, leftNode.children.remove(j+t)); 
+				rightNode.children.add(j, leftNode.children.remove(j+t - 1)); 
 			}
 		}
 
-		// Shift parent node's children to make space for new child 
-		/*for (int j = parent.children.size(); j > i; j--) {
-			parent.children.set(j+1, parent.children.get(j)); 
-		}*/
 		// Set new right node to be a child 
 		parent.children.add(i+1, rightNode);
 
-		// Shift parent values to make space for new element 
-		/*for (int j = parent.values.size() - 1; j >= i; j--) {
-			parent.values.set(j+1, parent.values.get(j)); 
-		} */
 		// Pull new element up from the left node 
 		parent.values.add(i, leftNode.values.remove(t - 1)); 
 	} 
     
     public void add(TreeObject element) {
     	// if BTree is empty
-    	if(nodes.size() == 0) {
+    	if(root.values.size() == 0) {
     		root = new BTreeNode(true, t);
     		root.values.add(0, element);
     	}
@@ -98,7 +48,7 @@ public class BTree {
     		// if root is full --> need to create new node and
         	if(root.isFull()) {
         		BTreeNode s = new BTreeNode(false, t); // make a new node
-        		s.children.set(0, root); 
+        		s.children.add(0, root); 
         		split(s, 0); //Should this be 0?
         		insertNonFull(s, element); 	
         		root = s;
