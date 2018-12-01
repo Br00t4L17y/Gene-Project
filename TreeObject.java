87486738b9e3e270a86c50e243eca445aa06fcf3
mustelidae1 <othm@live.com>
2018-12-01 11:31:28 -0700

    trying to merge AJ's code

--- TreeObject.java
+++ TreeObject.java
@@ -1,10 +1,8 @@
 public class TreeObject {
 	private Long key;
 	private int frequency;
-	private String sequence;
 
 	public TreeObject(String sequence){
-		this.sequence = sequence;
 		this.frequency = 0;
 		String binaryVal = "";
 		for(int i = 0; i < sequence.length(); i++){
@@ -38,18 +36,21 @@ public class TreeObject {
 	public int getFrequency(){
 		return this.frequency;
 	}
-
+	
+	/**
+	 * 
+	 * @param object
+	 * @return -1 if this.key < parameter object
+	 * @return 0 if the two keys are equal
+	 * @return 1 if this.key > the parameter object
+	 */
 	public int compareTo(TreeObject object){
-		if (object.getKey() < this.key) {
+		if (object.getKey() > this.key) {
 			return -1;
 		}
-		else if(object.getKey() == this.key) {
+		else if(object.getKey().equals(this.key)) {
 			return 0;
 		}
 		return 1;
-	}
-
-	public String toString(){
-		return this.sequence + ": " + this.frequency;
 	}
 }
